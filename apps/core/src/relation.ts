e8ed079df18a0663bef4fb4e5cc8e122e2478c8b
Hai Le <haile.vnm@gmail.com>
2023-11-24 15:35:02 +0700

    feat(core): add o.relation all cases

--- apps/core/src/relation.ts
+++ apps/core/src/relation.ts
@@ -2,9 +2,10 @@ import { CanIt } from '@can-it/core';
 import { RelationComparator } from '@can-it/operators-relation';
 
 const relationComparator = new RelationComparator(
-  ['view', 'edit', 'delete'],
+  ['view', 'edit', 'delete', '*'],
   {
-    edit: ['view']
+    edit: ['view'],
+    ['*']: [] // included all other actions
   }
 )
 
@@ -14,9 +15,11 @@ const canIt = new CanIt(
       ['view', 'docs'],
       ['edit', 'docs'],
       ['delete', 'docs'],
+      ['*', 'products'],
     ],
     deny: [
       ['edit', 'docs'],
+      ['delete', 'products'],
     ]
   },
   relationComparator
@@ -28,3 +31,8 @@ console.log('👉👉 CanIt with Relation Comparator Action');
 console.log(canIt.allowTo('view', 'docs')); // true, even the "edit" action was deny, and "view" is its children action, but we can perform the "view" action
 console.log(canIt.allowTo('delete', 'docs')); // true
 console.log(canIt.allowTo('edit', 'docs')); // false, because it's appeared in the "deny" permissions list
+
+console.log('- All action "*" case');
+console.log(canIt.allowTo('view', 'products')); // true
+console.log(canIt.allowTo('edit', 'products')); // true
+console.log(canIt.allowTo('delete', 'products')); // false, because it's appeared in the "deny" permissions list
