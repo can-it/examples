010b6f3c8c79817795f7b5105961c4492ae4de3d
Hai Le <haile.vnm@gmail.com>
2023-11-29 10:47:08 +0700

    feat(nest): setup authorization config

--- apps/nest/src/app/app.module.ts
+++ apps/nest/src/app/app.module.ts
@@ -1,11 +1,45 @@
 import { Module } from '@nestjs/common';
-
+import { CanItGuard, CanItModule } from '@can-it/nest';
+import { PolicyState } from '@can-it/types';
+import { Request } from 'express';
+import { RelationComparator } from '@can-it/operators-relation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    CanItModule.register({
+      comparators: {
+        action: new RelationComparator(
+          ['view', 'click'],
+          { click: ['view'] }
+        )
+      },
+      resolvers: {
+        ri: (req: Request) => req.params.id || req.path.split('/').pop(),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        policy: (_req: Request): PolicyState => {
+          return {
+            allow: [
+              ['view', 'cats'],
+              ['click', 'cats']
+            ],
+            deny: [
+              ['click', 'cats']
+            ]
+          }
+        }
+      }
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: CanItGuard
+    }
+  ],
 })
 export class AppModule {}
