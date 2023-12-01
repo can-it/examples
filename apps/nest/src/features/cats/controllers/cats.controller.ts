ba51a109b07e0b481f17efc26eb388613299b592
Hai Le <haile.vnm@gmail.com>
2023-12-01 17:08:35 +0700

    feat(nest): apply UseRiResolver

--- apps/nest/src/features/cats/controllers/cats.controller.ts
+++ apps/nest/src/features/cats/controllers/cats.controller.ts
@@ -1,6 +1,7 @@
-import { CanIt } from '@can-it/nest';
-import { Body, Controller, Delete, Get, Param, Patch, ValidationPipe } from '@nestjs/common';
+import { CanIt, UseRiResolver } from '@can-it/nest';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
 import { CatsService } from '../../../integrations/database/services/cats.service';
+import { Request } from 'express';
 
 @Controller('cats')
 @CanIt('view')
@@ -21,14 +22,20 @@ export class CatsController {
     return this.catsService.get(id);
   }
 
+  // The using of @UseRiResolver bellow will return the "cats" as resource identitfy
+  // you can implement your logic base on request object.
+  // That return values will combine as the "view" action provided from CanIt controller scope above
+  @UseRiResolver(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (_req: Request) => 'cats'
+  )
   @Patch(':id')
-  @CanIt('edit', 'cats')
   update(@Param('id') id: string, @Body('name') name: string) {
     return this.catsService.updateName(id, name);
   }
 
-  @CanIt('delete', 'cats')
   @Delete(':id')
+  @CanIt('delete', 'cats')
   delete(@Param('id') id: string) {
     return this.catsService.delete(id);
   }
