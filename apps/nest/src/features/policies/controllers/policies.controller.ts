9445278b6bd76b592cd675b2e1202b1571552672
Hai Le <haile.vnm@gmail.com>
2023-12-04 11:20:08 +0700

    feat(nest): use UsePolicyResolver decorator

--- apps/nest/src/features/policies/controllers/policies.controller.ts
+++ apps/nest/src/features/policies/controllers/policies.controller.ts
@@ -1,6 +1,15 @@
 import { PolicyState } from '@can-it/types';
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { PolicyService } from 'apps/nest/src/integrations/database/services/policy.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  ExecutionContext,
+  Get,
+  Post
+} from '@nestjs/common';
+import { PolicyService } from '../../../integrations/database/services/policy.service';
+import { CanIt, UsePolicyResolver } from '@can-it/nest';
+import { ModuleRef } from '@nestjs/core';
 
 @Controller('policies')
 export class PoliciesController {
@@ -17,5 +26,21 @@ export class PoliciesController {
   replace(@Body('policy') policy: PolicyState) {
     this.policyService.update(policy);
     return this.policyService.get();
+  }
+
+  @Delete(':actor')
+  @CanIt('DELETE', 'POLICIES')
+  @UsePolicyResolver(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (context: ExecutionContext, _thisModule: ModuleRef): PolicyState => {
+      const req = context.switchToHttp().getRequest();
+      return {
+        allow: req.params['actor'] === 'admin' ? [['DELETE', 'POLICIES']] : []
+      };
+    }
+  )
+  delete() {
+    this.policyService.update({ allow: [] });
+    return { message: 'Your permissions now is reset to empty, you need to add permission to fetching the cats'}
   }
 }
